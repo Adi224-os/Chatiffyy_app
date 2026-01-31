6304ea3f97b28c10d0f748777a90bb9f7a18017f
Adi224-os <adiveer224@gmail.com>
2026-01-31 08:07:44 +0530

    login and logout functionality added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -8,5 +8,6 @@ export const ENV = {
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
     EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+    CLIENT_URL: process.env.CLIENT_URL,
 };
 
